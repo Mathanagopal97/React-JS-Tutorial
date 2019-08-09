257221a5ac2f5642b434ab3af5dccfdb38744061
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-09 21:08:28 +0530

    Different datatypes in props

--- Properties/Hello.js
+++ Properties/Hello.js
@@ -1,8 +1,11 @@
 class Hello extends React.Component{
     render(){
+        console.log(this.props);
+        let bangs = "";
+        if(this.props.bangs) bangs = "!".repeat(this.props.bangs);
         return (
             <div>
-                <h1>Hello to {this.props.to} from {this.props.from}</h1>
+                <h1>Hello to {this.props.to} from {this.props.from}{bangs}</h1>
             </div>
         );
     }
--- Properties/index.js
+++ Properties/index.js
@@ -4,6 +4,13 @@ class App extends React.Component{
             <div>
                 <Hello to="Andy" from="Mathan"/>
                 <Hello to="Mathew" from="Steven"/>
+                <Hello 
+                    to = "Mathew" 
+                    from = "Steven"
+                    data = {[1,2,3,4,5,6]}
+                    isFunny
+                    bangs = {4}
+                />
             </div>
         );
     }

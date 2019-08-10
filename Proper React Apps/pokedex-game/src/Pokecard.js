44c8aff58ed07f27be225b5d46132083ddc3ad84
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-10 10:27:36 +0530

    Advanced Styling

--- Proper React Apps/pokedex-game/src/Pokecard.js
+++ Proper React Apps/pokedex-game/src/Pokecard.js
@@ -11,7 +11,7 @@ class Pokecard extends Component{
         return(
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{this.props.name}</h1>
-                <img src = {imgSRC}/>
+                <div className="Pokecard-image"><img src = {imgSRC}/></div>
                 <div className="Pokecard-data">Type: {this.props.type}</div>
                 <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>

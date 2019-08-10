0055902bcbcaa316ee06162dab7832b6acd943ef
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-10 08:55:48 +0530

    Basic Styles

--- Proper React Apps/pokedex-game/src/Pokecard.js
+++ Proper React Apps/pokedex-game/src/Pokecard.js
@@ -7,11 +7,10 @@ class Pokecard extends Component{
         var imgSRC = `${IMG_API}${this.props.id}.png`;
         return(
             <div className="Pokecard">
-                <h1>Pokecard!!!</h1>
-                <h2>{this.props.name}</h2>
+                <h1 className="Pokecard-title">{this.props.name}</h1>
                 <img src = {imgSRC}/>
-                <h3>Type: {this.props.type}</h3>
-                <h3>EXP: {this.props.exp}</h3>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         )
     }
